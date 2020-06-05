c139f1367faeb6de280e0754304b7689a90be782
Evgeny Danienko <6655321@bk.ru>
2020-06-05 16:15:01 +0300

    test

--- common/big_test.go
+++ common/big_test.go
@@ -19,9 +19,14 @@ func TestRightPadding(t *testing.T) {
 
 					withPadding := setUint256WithPadding(slice, l)
 					withoutPadding := setUint256(slice, l)
+					withoutPaddingDirect := setUint256WithoutPadding(slice)
 					if withoutPadding.Cmp(withPadding) != 0 {
 						t.Fatalf("not equal n=%d l=%d slice=%v\nGot\t\t%v\n!=\nExpect\t%v(%v)",
 							n, l, slice, withoutPadding.Bytes32(), withPadding.Bytes32(), spew.Sdump(withPadding))
+					}
+					if withoutPadding.Cmp(withoutPaddingDirect) != 0 {
+						t.Fatalf("not equal n=%d l=%d slice=%v\nGot\t\t%v\n!=\nExpect\t%v(%v)",
+							n, l, slice, withoutPadding.Bytes32(), withoutPaddingDirect.Bytes32(), spew.Sdump(withoutPaddingDirect))
 					}
 				}
 			}
@@ -31,6 +36,10 @@ func TestRightPadding(t *testing.T) {
 
 func setUint256WithPadding(slice []byte, l int) *uint256.Int {
 	return new(uint256.Int).SetBytes(RightPadBytes(slice, l))
+}
+
+func setUint256WithoutPadding(slice []byte) *uint256.Int {
+	return new(uint256.Int).SetBytes(slice)
 }
 
 func setUint256(slice []byte, l int) *uint256.Int {
