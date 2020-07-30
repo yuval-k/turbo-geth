d2eed7164c4e2db25c80af458c4c47787deb15c5
Igor Mandrigin <i@mandrigin.ru>
2020-07-30 17:08:23 +0200

    change LMBD map size

--- ethdb/kv_lmdb.go
+++ ethdb/kv_lmdb.go
@@ -15,7 +15,7 @@ import (
 	"github.com/ledgerwatch/turbo-geth/log"
 )
 
-const LMDBMapSize = 4 << 40 // 4TB
+const LMDBMapSize = 200 * 1024 * 1024 * 1024 // 200Gb
 
 type lmdbOpts struct {
 	path     string
