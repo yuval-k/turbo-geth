770c05b8b4aa5429af5cd07c4330509251ac3b09
alex.sharov <AskAlexSharov@gmail.com>
2020-09-03 16:19:32 +0700

    clean

--- eth/stagedsync/stage_interhashes.go
+++ eth/stagedsync/stage_interhashes.go
@@ -238,7 +238,7 @@ func incrementIntermediateHashes(s *StageState, db ethdb.Database, to uint64, da
 	}
 	c := tx.(ethdb.HasTx).Tx().CursorDupSort(dbutils.IntermediateTrieHashBucket)
 	del := func(k1, k2 []byte) error {
-		if k2 == nil {
+		if len(k2) == 0 {
 			v, err := c.SeekExact(k1)
 			if err != nil { // if key not found, or found another one - then nothing to delete
 				return err
@@ -387,7 +387,7 @@ func unwindIntermediateHashesStageImpl(u *UnwindState, s *StageState, db ethdb.D
 	buf.SetComparator(tx.(ethdb.HasTx).Tx().Comparator(dbutils.IntermediateTrieHashBucket))
 	collector := etl.NewCollector(datadir, buf)
 	del := func(k1, k2 []byte) error {
-		if k2 == nil {
+		if len(k2) == 0 {
 			v, err := c.SeekExact(k1)
 			if err != nil { // if key not found, or found another one - then nothing to delete
 				return err
