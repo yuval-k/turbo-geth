ecb74fd4796671d6fe6701ff5ade6974988dce5d
alex.sharov <AskAlexSharov@gmail.com>
2020-10-14 10:53:20 +0700

    linter fix

--- cmd/integration/commands/stages.go
+++ cmd/integration/commands/stages.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"context"
+	"github.com/ledgerwatch/turbo-geth/core/rawdb"
 	"runtime"
 	"time"
 
@@ -505,7 +506,7 @@ func stageTxLookup(db ethdb.Database, ctx context.Context) error {
 	return stagedsync.SpawnTxLookup(stage9, db, datadir, ch)
 }
 
-func printAllStages(db ethdb.Database, _ context.Context) error {
+func printAllStages(db rawdb.DatabaseReader, _ context.Context) error {
 	return printStages(db)
 }
 
