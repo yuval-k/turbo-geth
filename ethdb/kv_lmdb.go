7fb3cf85605c3de64216d1472124ceb43ffa7958
alex.sharov <AskAlexSharov@gmail.com>
2020-10-15 10:44:42 +0700

    print freelist in bucketsats

--- ethdb/kv_lmdb.go
+++ ethdb/kv_lmdb.go
@@ -666,7 +666,7 @@ func (tx *lmdbTx) BucketSize(name string) (uint64, error) {
 }
 
 func (tx *lmdbTx) BucketStat(name string) (*lmdb.Stat, error) {
-	if name == "freelist" || name == "gc" || name == "free_list" {
+	if name == "freelist" || name == "gc" || name == "free_list" { //nolint:goconst
 		return tx.tx.Stat(lmdb.DBI(0))
 	}
 	return tx.tx.Stat(lmdb.DBI(tx.db.buckets[name].DBI))
