5e1bb7479d4edce56cf09b4ba492f12418ec5698
alex.sharov <AskAlexSharov@gmail.com>
2020-10-31 09:09:27 +0700

    mdbx_exclusive_open_for_migrations

--- ethdb/kv_mdbx.go
+++ ethdb/kv_mdbx.go
@@ -497,51 +497,51 @@ func (tx *mdbxTx) dropEvenIfBucketIsNotDeprecated(name string) error {
 		}
 		dbi = dbutils.DBI(nativeDBI)
 	}
-	logEvery := time.NewTicker(30 * time.Second)
-	defer logEvery.Stop()
-	for {
-		s, err := tx.BucketStat(name)
-		if err != nil {
-			return err
-		}
-		if s.Entries < 100_000 {
-			break
-		}
-		c := tx.Cursor(name)
-		i := 0
-		var k []byte
-		for k, _, err = c.First(); k != nil; k, _, err = c.First() {
-			if err != nil {
-				return err
-			}
-			err = c.DeleteCurrent()
-			if err != nil {
-				return err
-			}
-			i++
-			if i > 100_000 {
-				break
-			}
-
-			select {
-			default:
-			case <-logEvery.C:
-				log.Info("dropping bucket", "name", name, "current key", fmt.Sprintf("%x", k))
-			}
-		}
-
-		c.Close()
-		_, err = tx.tx.Commit()
-		if err != nil {
-			return err
-		}
-		txn, err := tx.db.env.BeginTxn(nil, mdbx.TxRW)
-		if err != nil {
-			return err
-		}
-		txn.RawRead = true
-		tx.tx = txn
-	}
+	//logEvery := time.NewTicker(30 * time.Second)
+	//defer logEvery.Stop()
+	//for {
+	//	s, err := tx.BucketStat(name)
+	//	if err != nil {
+	//		return err
+	//	}
+	//	if s.Entries < 100_000 {
+	//		break
+	//	}
+	//	c := tx.Cursor(name)
+	//	i := 0
+	//	var k []byte
+	//	for k, _, err = c.First(); k != nil; k, _, err = c.First() {
+	//		if err != nil {
+	//			return err
+	//		}
+	//		err = c.DeleteCurrent()
+	//		if err != nil {
+	//			return err
+	//		}
+	//		i++
+	//		if i > 100_000 {
+	//			break
+	//		}
+	//
+	//		select {
+	//		default:
+	//		case <-logEvery.C:
+	//			log.Info("dropping bucket", "name", name, "current key", fmt.Sprintf("%x", k))
+	//		}
+	//	}
+	//
+	//	c.Close()
+	//	_, err = tx.tx.Commit()
+	//	if err != nil {
+	//		return err
+	//	}
+	//	txn, err := tx.db.env.BeginTxn(nil, mdbx.TxRW)
+	//	if err != nil {
+	//		return err
+	//	}
+	//	txn.RawRead = true
+	//	tx.tx = txn
+	//}
 	fmt.Printf("DropL %s\n", name)
 	if err := tx.tx.Drop(mdbx.DBI(dbi), true); err != nil {
 		return err
